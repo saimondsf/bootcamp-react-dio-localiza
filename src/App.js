acad44a7bcd91d74db6bfd2f078492926b59f7b3
Saimon Davi <saimon.dsfg@gmail.com>
2021-08-17 19:55:18 -0300

    Using the library 'styled-components'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Item from './components/Item';
 import Card from './components/Card';
+import styled from 'styled-components'; //npm install --save styled-components
+
 
 const App = () => {
   return (
-    <>
+    <Content>
       <h1>Aplicação React</h1>
       <ul>
       <Item>Item 1</Item>
@@ -11,8 +13,13 @@ const App = () => {
       <Item>Item 3</Item>
       </ul>
       <Card/>
-    </>
+    </Content>
   )
 }
+
+//Utilizando a biblioteca styled-components
+const Content = styled.div` 
+  color: green;
+`;
 
 export default App;
