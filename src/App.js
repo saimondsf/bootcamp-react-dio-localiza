f4b5bbc6f95d0e4395ea56791961595020a7b791
Saimon Davi <saimon.dsfg@gmail.com>
2021-08-24 22:06:00 -0300

    Projeto Pesquisa GitHub com ReactJS - Segundo Projeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import IfoodCounter from './components/IfoodCounter/IfoodCounter';
+import React from 'react';
+import Layout from './components/layout';
+import NoSearch from './components/no-search';
+import Profile from './components/profile';
+import Repositories from './components/repositories';
+import useGithub from './hooks/github-hooks';
 
 const App = () => {
+  const { githubState } = useGithub();
   return (
-    <>
-      <h1>Ifood</h1>
-      <IfoodCounter/>
-    </>
-  )
-}
-
+    <Layout>
+      {githubState.hasUser ? 
+        (<>{githubState.loading ? (<p>Loading</p>) : (<><Profile /><Repositories /></>)}</>) 
+        : 
+        (<NoSearch />)
+      }
+    </Layout>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Providers from './providers';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Providers />
+  </React.StrictMode>,
   document.getElementById('root')
 );
